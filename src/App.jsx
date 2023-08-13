ab1c051a45eabdea54afc16f851316a5c7e3d1b6
nurealammiaji <nurealam.miaji@gmail.com>
2023-08-13 14:35:52 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-center text-green-500">
+        Welcome to JobNest !!
+      </h1>
+    </>
+  )
+}
+
+export default App;
