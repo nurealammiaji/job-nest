e42c68856f73eb41a91baba7c85cccddfa503f5f
nurealammiaji <nurealam.miaji@gmail.com>
2023-08-14 01:03:40 +0600

    featured jobs section added

--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,21 +1,25 @@
+import { MapPinIcon, CurrencyDollarIcon } from '@heroicons/react/24/solid';
+
+
 const Job = ({job}) => {
 
     const {title, company, image, type, location, salary} = job;
     console.log(job);
 
     return (
-        <div>
+        <div className='p-5 border rounded-md'>
             <div>
                 <img src={image} alt="" />
             </div>
-            <h4 className="text-xl">{title}</h4>
-            <p>{company}</p>
-            <div>
-                <span>{type[0]}</span><span>{type[1]}</span>
+            <h4 className="mt-5 text-xl">{title}</h4>
+            <p className='my-2'>{company}</p>
+            <div className='my-5 [&>*]:mr-2'>
+                <small className='p-2 border border-blue-500'>{type[0]}</small><small className='p-2 border border-blue-500'>{type[1]}</small>
             </div>
-            <div>
-                <span>{location}</span><span>{salary}</span>
+            <div className='flex items-center [&>*]:m-1 text-gray-500'>
+            <MapPinIcon className="w-6 h-6" /><small>{location}</small><CurrencyDollarIcon className="w-6 h-6" /><small>{salary}</small>
             </div>
+            <button className='px-4 py-2 mt-3 font-semibold text-white bg-blue-500 rounded-md'>View Details</button>
         </div>
     );
 };
