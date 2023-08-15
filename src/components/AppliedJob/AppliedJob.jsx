32b3d238438d72e74de15b9f0d72d917ec569489
nurealammiaji <nurealam.miaji@gmail.com>
2023-08-15 06:09:56 +0600

    applied jobs responsive for mobile

--- src/components/AppliedJob/AppliedJob.jsx
+++ src/components/AppliedJob/AppliedJob.jsx
@@ -1,3 +1,4 @@
+import { MapPinIcon, CurrencyDollarIcon } from '@heroicons/react/24/solid';
 import { Link } from "react-router-dom";
 
 const AppliedJob = ({job}) => {
@@ -12,18 +13,19 @@ const AppliedJob = ({job}) => {
                 <img className="w-40" src={image} alt="" />
             </div>
             <div className="grow">
-                <h4 className="text-xl">{title}</h4>
-                <p>{company}</p>
-                <div>
-                    <small>{type}</small>
+                <h4 className="mt-5 text-xl md:mt-0">{title}</h4>
+                <p className='my-2'>{company}</p>
+                <div className='my-5 [&>*]:mr-2 text-blue-500'>
+                    <small className='p-2 border border-blue-500'>{type[0]}</small>
+                    <small className='p-2 border border-blue-500'>{type[1]}</small>
                 </div>
-                <div>
-                    <small>{location}</small>
-                    <small>{salary}</small>
+                <div className='flex items-center [&>*]:mr-1 text-gray-500'>
+                <MapPinIcon className="w-6 h-6" /><small>{location}</small>
+                <CurrencyDollarIcon className="w-6 h-6" /><small>{salary}</small>
                 </div>
             </div>
             <div>
-                <Link to={`/job/${job_id}`}><button className="px-5 py-2 font-semibold text-white bg-blue-500 rounded-md">View Details</button></Link>
+                <Link to={`/job/${job_id}`}><button className='px-4 py-2 mt-4 mt-5 text-sm font-semibold text-white bg-blue-500 rounded md:mt-0'>View Details</button></Link>
             </div>
         </div>
     );
