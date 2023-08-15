752b0af87ad726c7fd1857f83159cae0bf48b18f
nurealammiaji <nurealam.miaji@gmail.com>
2023-08-15 15:12:16 +0600

    banner added

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,7 +1,17 @@
+import vector from "../../../public/assets/images/Vector.png";
+
 const Blog = () => {
     return (
-        <div className="px-5">
-            <h3>Blog Page</h3>
+        <div>
+            <div className="bg-blue-50">
+                <br /><br />
+                <h3 className="ml-5 text-3xl font-bold md:ml-0 md:text-center">Blog</h3>
+                <br />
+                <img className="w-4/12 mt-10 md:mt-0 md:w-2/12" src={vector} alt="" />
+            </div>
+            <div className="p-10">
+                <h5>Blog Will here ...</h5>
+            </div>
         </div>
     );
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,7 +1,17 @@
+import vector from "../../../public/assets/images/Vector.png";
+
 const Statistics = () => {
     return (
-        <div className="px-5">
-            <h3>Statistics Page</h3>
+        <div>
+            <div className="bg-blue-50">
+                <br /><br />
+                <h3 className="ml-5 text-3xl font-bold md:ml-0 md:text-center">Statistics</h3>
+                <br />
+                <img className="w-4/12 mt-10 md:mt-0 md:w-2/12" src={vector} alt="" />
+            </div>
+            <div className="p-10">
+                <h5>Chart Will here ...</h5>
+            </div>
         </div>
     );
 };
